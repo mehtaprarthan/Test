61886ec1305cde2f4ec690b783ad619d3f46df10
LD <LD@DESKTOP-BG61G85.gateway.sasktel.net>
2021-01-30 17:13:03 -0700

    For GIT Testing

---
+++ GITTest/src/main/java/com/Sample/GITTest/App.java
@@ -0,0 +1,13 @@
+package com.Sample.GITTest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! This is GIT Test" );
+    }
+}
---
+++ GITTest/src/test/java/com/Sample/GITTest/AppTest.java
@@ -0,0 +1,20 @@
+package com.Sample.GITTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
